26b66b64d5e0b17c59839f5d53af5ee525468b71
Quentin Marciset <quentin.marciset@4d.com>
2020-02-10 13:49:45 +0100

    cleaned dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -14,12 +14,10 @@ object Versions {
     val android_gradle_plugin = "3.5.2"
     val artifactory = "4.13.0"
     val atsl_junit = "1.1.1"
-    val constraint_layout = "2.0.0-beta4"
-    val design = "1.0.0"
     val junit = "4.13"
     val kotlin = "1.3.61"
+    val lifecycle = "2.2.0"
     val mockito = "3.2.4"
-    val navigation = "2.2.0"
     val okhttp = "4.3.1"
     val preference = "1.1.0"
     val retrofit = "2.7.1"
@@ -57,21 +55,16 @@ object Libs {
 
     // Common
     val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.support}"
-    val androidx_constraintlayout =
-        "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
-    val androidx_core = "androidx.core:core-ktx:${Versions.support}"
-    val androidx_recyclerview = "androidx.recyclerview:recyclerview:${Versions.support}"
     val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
 
-    // Utils
-    val material = "com.google.android.material:material:${Versions.design}"
-    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+    // Lifecycle
+    val lifecycle_extensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
 
-    // Navigation
-    val androidx_navigation_fragment =
-        "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
-    val androidx_navigation_ui =
-        "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+    // Utils
+    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava2}"
+    val timber = "com.jakewharton.timber:timber:${Versions.timber}"
 
     // Testing
     val junit = "junit:junit:${Versions.junit}"
@@ -80,18 +73,13 @@ object Libs {
     val androidx_junit = "androidx.test.ext:junit:${Versions.atsl_junit}"
     val androidx_preference_ktx = "androidx.preference:preference-ktx:${Versions.preference}"
     val mockito = "org.mockito:mockito-core:${Versions.mockito}"
-    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
     val okhttp_logging_interceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
     val okhttp_mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.okhttp}"
     val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
     val retrofit_adapter_rxjava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
-    val retrofit_converter_gson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
     val robolectric = "org.robolectric:robolectric:${Versions.robolectric}"
     val rxandroid = "io.reactivex.rxjava2:rxandroid:${Versions.rx_android}"
-    val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjava2}"
 
     // For AndroidMobileDataStore
     val androidx_room = "androidx.room:room-ktx:${Versions.room}"
-    val androidx_room_runtime = "androidx.room:room-runtime:${Versions.room}"
-    val androidx_room_compiler = "androidx.room:room-compiler:${Versions.room}"
 }
