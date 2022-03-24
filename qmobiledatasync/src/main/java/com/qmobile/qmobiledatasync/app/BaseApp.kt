653852230be1941157c728b5fb169f7a83de0409
htemanni <houssam.temanni@4D.com>
2022-03-24 10:26:08 +0100

    move shouldShowActionError to BaseApp

--- qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/app/BaseApp.kt
+++ qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/app/BaseApp.kt
@@ -17,6 +17,11 @@ import com.qmobile.qmobiledatasync.utils.GenericRelationHelper
 import com.qmobile.qmobiledatasync.utils.GenericTableFragmentHelper
 import com.qmobile.qmobiledatasync.utils.GenericTableHelper
 import com.qmobile.qmobiledatasync.utils.RuntimeDataHolder
+import java.util.Date
+
+const val MILLISECONDS_IN_SECONDS = 1000
+const val SECONDS_IN_MINUTE = 60
+
 
 open class BaseApp : MultiDexApplication() {
 
@@ -58,6 +63,21 @@ open class BaseApp : MultiDexApplication() {
         } else {
             false
         }
+
+        fun shouldShowActionError(): Boolean {
+            var sharedPreferencesHolder = BaseApp.sharedPreferencesHolder
+            val lastDisplayErrorTime =
+                Date(sharedPreferencesHolder.lastTimeActionErrorDisplayed).time
+            val diffInSeconds = (Date().time - lastDisplayErrorTime) / MILLISECONDS_IN_SECONDS
+
+            if (diffInSeconds >= SECONDS_IN_MINUTE) {
+                // reset lastTimeActionErrorDisplayed with the currentTime
+                sharedPreferencesHolder.lastTimeActionErrorDisplayed = Date().time
+                return true
+            }
+            return false
+        }
+
     }
 
     override fun onCreate() {
