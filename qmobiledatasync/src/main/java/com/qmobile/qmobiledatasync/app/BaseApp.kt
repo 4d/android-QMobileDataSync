bafc74ee516904d928e849d27968a2a68689426e
htemanni <houssam.temanni@4D.com>
2022-04-19 14:54:49 +0200

    code review

--- qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/app/BaseApp.kt
+++ qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/app/BaseApp.kt
@@ -62,20 +62,6 @@ open class BaseApp : MultiDexApplication() {
         } else {
             false
         }
-
-        fun shouldShowActionError(): Boolean {
-            var sharedPreferencesHolder = BaseApp.sharedPreferencesHolder
-            val lastDisplayErrorTime =
-                Date(sharedPreferencesHolder.lastTimeActionErrorDisplayed).time
-            val diffInSeconds = (Date().time - lastDisplayErrorTime) / MILLISECONDS_IN_SECONDS
-
-            if (diffInSeconds >= SECONDS_IN_MINUTE) {
-                // reset lastTimeActionErrorDisplayed with the currentTime
-                sharedPreferencesHolder.lastTimeActionErrorDisplayed = Date().time
-                return true
-            }
-            return false
-        }
     }
 
     override fun onCreate() {
