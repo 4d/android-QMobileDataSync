b020bf962645f7b78529eb12cdca5dba67503a1a
Quentin Marciset <quentin.marciset@4d.com>
2022-06-10 09:56:52 +0200

    fix TU

--- qmobiledatasync/src/test/java/com/qmobile/qmobiledatasync/RelationHelperTest.kt
+++ qmobiledatasync/src/test/java/com/qmobile/qmobiledatasync/RelationHelperTest.kt
@@ -9,10 +9,7 @@ package com.qmobile.qmobiledatasync
 import android.app.Application
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider
-import com.qmobile.qmobileapi.model.entity.Entities
-import com.qmobile.qmobileapi.model.entity.EntityModel
 import com.qmobile.qmobileapi.utils.getSafeObject
-import com.qmobile.qmobiledatasync.utils.ReflectionUtils
 import org.json.JSONObject
 import org.junit.Assert
 import org.junit.Before
@@ -23,13 +20,10 @@ import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import kotlin.reflect.full.declaredMemberProperties
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Build.VERSION_CODES.P])
 class RelationHelperTest {
-
-    private val prefixForTests = "com.qmobile.qmobiledatasync."
 
     @Mock
     lateinit var application: Application
@@ -41,73 +35,6 @@ class RelationHelperTest {
         Mockito.`when`(application.packageName).thenReturn(
             ApplicationProvider.getApplicationContext<Application>().packageName.removeSuffix(".test")
         )
-    }
-
-    @Test
-    fun isManyToOneRelation() {
-
-        val tableNames =
-            listOf("Table1", "Table2", " ${prefixForTests}Employee", " ${prefixForTests}Service")
-
-        data class CustomEmployee(
-            val serviceProperty: Service? = null,
-            override val __KEY: String,
-            override val __STAMP: Int? = null,
-            override val __GlobalStamp: Int? = null,
-            override val __TIMESTAMP: String? = null
-        ) : EntityModel
-
-        val properties: Collection<*>
-        properties = CustomEmployee::class.declaredMemberProperties
-
-        properties.toList().forEach { property ->
-
-            val manyToOneRelation = ReflectionUtils.getManyToOneRelation(
-                property,
-                application,
-                tableNames
-            )
-
-            if (property.toString().contains("serviceProperty")) {
-                Assert.assertEquals(" ${prefixForTests}Service", manyToOneRelation)
-            } else {
-                Assert.assertNull(manyToOneRelation)
-            }
-        }
-    }
-
-    @Test
-    fun isOneToManyRelation() {
-
-        val tableNames =
-            listOf("Table1", "Table2", "${prefixForTests}Employee", "${prefixForTests}Service")
-
-        data class CustomEmployee(
-            val serviceProperty: Service? = null,
-            val employeesProperty: Entities<Employee>? = null,
-            override val __KEY: String,
-            override val __STAMP: Int? = null,
-            override val __GlobalStamp: Int? = null,
-            override val __TIMESTAMP: String? = null
-        ) : EntityModel
-
-        val properties: Collection<*>
-        properties = CustomEmployee::class.declaredMemberProperties
-
-        properties.toList().forEach { property ->
-
-            val oneToManyRelation = ReflectionUtils.getOneToManyRelation(
-                property,
-                application,
-                tableNames
-            )
-
-            if (property.toString().contains("employeesProperty")) {
-                Assert.assertEquals("${prefixForTests}Employee", oneToManyRelation)
-            } else {
-                Assert.assertNull(oneToManyRelation)
-            }
-        }
     }
 
     @Test
