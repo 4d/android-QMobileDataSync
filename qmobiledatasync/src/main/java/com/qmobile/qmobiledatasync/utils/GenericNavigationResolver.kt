2341661a32621c1ec2f865a3de4005c75c64c6d3
htemanni <houssam.temanni@4D.com>
2022-09-02 12:49:06 +0200

    az:4381 implement javascript injecttion from app to web view

--- qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/utils/GenericNavigationResolver.kt
+++ qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/utils/GenericNavigationResolver.kt
@@ -71,5 +71,5 @@ interface GenericNavigationResolver {
      */
     fun navigateToPendingTasks(fragmentActivity: FragmentActivity, tableName: String, currentItemId: String)
 
-    fun navigateToActionWebView(viewDataBinding: ViewDataBinding, path: String)
+    fun navigateToActionWebView(viewDataBinding: ViewDataBinding, path: String, actionName: String, actionLabel: String?, actionShortLabel: String?)
 }
