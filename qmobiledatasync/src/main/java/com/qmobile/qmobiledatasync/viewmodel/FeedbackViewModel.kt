776b3d9df9d84e8c4af2bb13356f3c80267f2464
Quentin Marciset <quentin.marciset@4d.com>
2023-01-11 11:53:57 +0100

    fix issue with sending @multimap

--- qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/viewmodel/FeedbackViewModel.kt
+++ qmobiledatasync/src/main/java/com/qmobile/qmobiledatasync/viewmodel/FeedbackViewModel.kt
@@ -8,10 +8,9 @@ package com.qmobile.qmobiledatasync.viewmodel
 
 import com.qmobile.qmobileapi.network.FeedbackApiService
 import com.qmobile.qmobileapi.repository.FeedbackRepository
-import com.qmobile.qmobileapi.utils.APP_JSON
-import com.qmobile.qmobileapi.utils.UTF8_CHARSET
 import com.qmobile.qmobileapi.utils.getSafeBoolean
 import com.qmobile.qmobileapi.utils.getSafeString
+import com.qmobile.qmobileapi.utils.retrieveJSONObject
 import com.qmobile.qmobiledatasync.app.BaseApp
 import com.qmobile.qmobiledatasync.log.LogFileHelper
 import com.qmobile.qmobiledatasync.toast.ToastMessage
@@ -19,9 +18,9 @@ import com.qmobile.qmobiledatasync.utils.FeedbackType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.json.JSONObject
 import timber.log.Timber
 import java.io.File
 
@@ -56,13 +55,13 @@ class FeedbackViewModel(feedbackApiService: FeedbackApiService) : BaseViewModel(
     }
 
     fun sendCrash(zipFile: File?, onResult: (isSuccess: Boolean, ticket: String?) -> Unit) {
-        val jsonBody = buildRequestJson(type = FeedbackType.REPORT_PREVIOUS_CRASH, hasFile = zipFile != null)
-        sendFeedbackAndLogs(jsonBody, zipFile, onResult)
+        val map = buildRequest(type = FeedbackType.REPORT_PREVIOUS_CRASH, hasFile = zipFile != null)
+        sendFeedbackAndLogs(map, zipFile, onResult)
     }
 
     fun sendCurrentLogs(zipFile: File?, onResult: (isSuccess: Boolean) -> Unit) {
-        val jsonBody = buildRequestJson(type = FeedbackType.SHOW_CURRENT_LOG, hasFile = zipFile != null)
-        sendFeedbackAndLogs(jsonBody, zipFile) { isSuccess, _ ->
+        val map = buildRequest(type = FeedbackType.SHOW_CURRENT_LOG, hasFile = zipFile != null)
+        sendFeedbackAndLogs(map, zipFile) { isSuccess, _ ->
             onResult(isSuccess)
         }
     }
@@ -74,28 +73,34 @@ class FeedbackViewModel(feedbackApiService: FeedbackApiService) : BaseViewModel(
         zipFile: File?,
         onResult: (isSuccess: Boolean) -> Unit
     ) {
-        val jsonBody = buildRequestJson(type, email, feedbackContent, zipFile != null)
-        sendFeedbackAndLogs(jsonBody, zipFile) { isSuccess, _ ->
+        val map = buildRequest(type, email, feedbackContent, zipFile != null)
+        sendFeedbackAndLogs(map, zipFile) { isSuccess, _ ->
             onResult(isSuccess)
         }
     }
 
     private fun sendFeedbackAndLogs(
-        jsonBody: JSONObject,
+        map: MutableMap<String, RequestBody>,
         zipFile: File?,
         onResult: (isSuccess: Boolean, ticket: String?) -> Unit
     ) {
-        if (zipFile == null) {
-            Timber.e("Cannot send crash log : zip file is null !")
-            return
+        var filePart: MultipartBody.Part? = null
+        zipFile?.let {
+            val requestFile = it.asRequestBody("multipart/form-data".toMediaType())
+            filePart = MultipartBody.Part.createFormData("file", it.name, requestFile)
         }
-        val body = jsonBody.toString().toRequestBody("$APP_JSON; $UTF8_CHARSET".toMediaTypeOrNull())
-        val requestFile = zipFile.asRequestBody("multipart/form-data".toMediaType())
-        val filePart = MultipartBody.Part.createFormData("file", zipFile.name, requestFile)
 
-        feedbackRepository.sendFeedbackAndLogs(body, filePart) { isSuccess, response, error ->
+        feedbackRepository.send(map, filePart) { isSuccess, response, error ->
             if (isSuccess) {
-                onResult(true, response?.body()?.string())
+                response?.body()?.let { responseBody ->
+                    retrieveJSONObject(responseBody.string())?.let { responseJson ->
+                        if (responseJson.getSafeBoolean("ok") == true) {
+                            onResult(true, responseJson.getSafeString("ticket"))
+                            return@send
+                        }
+                    }
+                }
+                onResult(false, null)
             } else {
                 treatFailure(response, error, "FeedbackViewModel")
                 onResult(false, null)
@@ -103,41 +108,41 @@ class FeedbackViewModel(feedbackApiService: FeedbackApiService) : BaseViewModel(
         }
     }
 
-    private fun buildRequestJson(
+    private fun createPartFromString(stringData: String): RequestBody {
+        return stringData.toRequestBody("text/plain".toMediaTypeOrNull())
+    }
+
+    private fun buildRequest(
         type: FeedbackType,
         email: String? = null,
         feedbackContent: String? = null,
         hasFile: Boolean
-    ): JSONObject {
-        return JSONObject().apply {
-            /*
-                build ?
-                component ?
-                ide ?
+    ): MutableMap<String, RequestBody> {
+        return mutableMapOf<String, RequestBody>().apply {
+            email?.let { put("email", createPartFromString(it)) }
+            feedbackContent?.let { put("summary", createPartFromString(it)) }
+            put("type", createPartFromString(type.key))
+            put("fileName", createPartFromString(if (hasFile) LogFileHelper.zipFileName else ""))
+            put("SendDate", createPartFromString(LogFileHelper.getCurrentDateTimeLogFormat()))
+            put("isCrash", createPartFromString(if (type == FeedbackType.REPORT_PREVIOUS_CRASH) "1" else "0"))
 
-                "Build": "100058",
-                "Component": "100058",
-                "IDE": "1980",
-            */
-
-            email?.let { put("email", it) }
-            feedbackContent?.let { put("summary", it) }
-            put("type", type.key)
-            put("fileName", if (hasFile) LogFileHelper.zipFileName else "")
-            put("SendDate", LogFileHelper.getCurrentDateTimeLogFormat())
-            put("isCrash", if (type == FeedbackType.REPORT_PREVIOUS_CRASH) "1" else "0")
-
-            BaseApp.sharedPreferencesHolder.appInfo.getSafeString("id")?.let { put("CFBundleIdentifier", it) }
-            BaseApp.sharedPreferencesHolder.appInfo.getSafeString("name")?.let { put("CFBundleName", it) }
+            BaseApp.sharedPreferencesHolder.appInfo.getSafeString("id")
+                ?.let { put("CFBundleIdentifier", createPartFromString(it)) }
+            BaseApp.sharedPreferencesHolder.appInfo.getSafeString("name")
+                ?.let { put("CFBundleName", createPartFromString(it)) }
             BaseApp.sharedPreferencesHolder.appInfo.getSafeString("version")
-                ?.let { put("CFBundleShortVersionString", it) }
-            BaseApp.sharedPreferencesHolder.device.getSafeString("version")?.let { put("DTPlatformVersion", it) }
-            BaseApp.sharedPreferencesHolder.device.getSafeString("id")?.let { put("uuid", it) }
-            BaseApp.sharedPreferencesHolder.device.getSafeString("description")?.let { put("device.description", it) }
-            BaseApp.sharedPreferencesHolder.device.getSafeBoolean("simulator")?.let { put("device.simulator", it) }
-            BaseApp.sharedPreferencesHolder.team.getSafeString("id")?.let { put("AppIdentifierPrefix", it) }
+                ?.let { put("CFBundleShortVersionString", createPartFromString(it)) }
+            BaseApp.sharedPreferencesHolder.device.getSafeString("version")
+                ?.let { put("DTPlatformVersion", createPartFromString(it)) }
+            BaseApp.sharedPreferencesHolder.device.getSafeString("id")?.let { put("uuid", createPartFromString(it)) }
+            BaseApp.sharedPreferencesHolder.device.getSafeString("description")
+                ?.let { put("device.description", createPartFromString(it)) }
+            BaseApp.sharedPreferencesHolder.device.getSafeBoolean("simulator")
+                ?.let { put("device.simulator", createPartFromString(it.toString())) }
+            BaseApp.sharedPreferencesHolder.team.getSafeString("id")
+                ?.let { put("AppIdentifierPrefix", createPartFromString(it)) }
 
-            put("sdk", BaseApp.runtimeDataHolder.sdkVersion)
+            put("sdk", createPartFromString(BaseApp.runtimeDataHolder.sdkVersion))
         }
     }
 
